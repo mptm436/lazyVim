1dd72f2f472702965be9dd81e1eb2e2cc44bec40
mptm436 <liqiancn@gmail.com>
2023-12-17 01:16:45 +0800

    feat: add custom keymaps and autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+-- Fixes Autocomment
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  callback = function()
+    vim.cmd("set formatoptions-=cro")
+  end,
+})
+
+-- Set wrap and spell in markdown and gitcommit
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,10 @@ local opts = { noremap = true, silent = true }
 
 keymap("i", "jj", "<Esc>:w<CR>", opts)
 keymap("i", "jk", "<Esc>:w<CR>", opts)
+
+-- greatest remap ever
+keymap("x", "<leader>p", [["_dP]])
+-- next greatest remap ever : asbjornHaland
+keymap({ "n", "v" }, "<leader>y", [["+y]])
+keymap("n", "<leader>Y", [["+Y]])
+keymap({ "n", "v" }, "<leader>d", [["_d]])
