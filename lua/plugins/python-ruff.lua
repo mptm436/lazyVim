16aa29b8760f0d00f5018e3cd002a19b68949e1a
mptm436 <liqiancn@gmail.com>
2023-12-20 16:32:21 +0800

    fix: change python linting and formatting to ruff

---
+++ lua/plugins/python-ruff.lua
@@ -0,0 +1,22 @@
+return {
+  -- Mason configuration for ensuring formatters are installed
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "ruff")
+      -- table.insert(opts.ensure_installed, "stylua")
+      -- table.insert(opts.ensure_installed, "prettierd")
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, nls.builtins.formatting.ruff)
+      table.insert(opts.sources, nls.builtins.diagnostics.ruff)
+      table.insert(opts.sources, nls.builtins.code_actions.ruff)
+    end,
+  },
+}
