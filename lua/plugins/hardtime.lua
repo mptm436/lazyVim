a2298715c8d1708ba83d6d5e0b9e6e2282afdb67
mptm436 <liqiancn@gmail.com>
2024-01-10 15:27:39 +0000

    fix: minor warning issue on hardtime plugin

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,7 +1,7 @@
 return {
   "m4xshen/hardtime.nvim",
-  enable = false,
-  command = "Hardtime",
+  -- enable = false,
+  -- command = "Hardtime",
   event = "BufEnter",
   keys = { { "<leader>uh", "<cmd>Hardtime toggle<cr>", desc = "Hardtime Toggle" } },
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
